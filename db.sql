c4c557bb79b6b06a2dcd0c01f6a5b69d06cd28ef
Duco Winterwerp <duco@winterwerp.it>
2015-12-28 22:46:16 +0100

    Added SQL script and .gitiginore

---
+++ db.sql
@@ -0,0 +1,53 @@
+/*
+SQLyog Community v12.09 (64 bit)
+MySQL - 5.6.25 : Database - urlsh2
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`urlsh2` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+USE `urlsh2`;
+
+/*Table structure for table `stats` */
+
+DROP TABLE IF EXISTS `stats`;
+
+CREATE TABLE `stats` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `url_id` int(11) NOT NULL,
+  `clickdate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `ip` varchar(100) NOT NULL,
+  `referer` varchar(1000) NOT NULL,
+  `user_agent_string` varchar(1000) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `url_id` (`url_id`),
+  CONSTRAINT `stats_ibfk_1` FOREIGN KEY (`url_id`) REFERENCES `urls` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=302 DEFAULT CHARSET=latin1;
+
+/*Table structure for table `urls` */
+
+DROP TABLE IF EXISTS `urls`;
+
+CREATE TABLE `urls` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `url` varchar(1000) NOT NULL,
+  `segment` varchar(15) NOT NULL,
+  `datetime_added` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `ip` varchar(25) NOT NULL,
+  `num_of_clicks` int(11) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `segment` (`segment`)
+) ENGINE=InnoDB AUTO_INCREMENT=118 DEFAULT CHARSET=latin1;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
